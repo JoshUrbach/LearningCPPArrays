cd27139dddd51eefe8ef7658cfef5bca2e612732
Josh Urbach <98100405+JoshUrbach@users.noreply.github.com>
2022-05-08 15:53:16 -0400

    Create subarraySum3.cpp
    Kadane's Algorithm

---
+++ subarraySum3.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+using namespace std;
+
+//Kadane's Algorithm O(N) time/space
+int maximum_subarray_sum(int arr[], int n){
+
+    int cs = 0;
+    int largest = 0;
+
+    for(int i = 0; i < n; i++){
+        cs = cs + arr[i];
+        if(cs < 0){
+            cs=0;
+        }
+
+        largest = max(largest, cs);
+    }
+
+    return largest;
+}
+
+
+
+int main() {
+	// your code goes here
+
+    int arr[] = {-2, 3, 4, -1, 5, -12, 6, 1, 3};
+    int n = sizeof(arr)/sizeof(int);
+
+    cout << maximum_subarray_sum(arr, n) << endl; 
+
+	return 0;
+}
